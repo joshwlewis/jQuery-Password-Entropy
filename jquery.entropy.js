247871b7f32b8dfbbba720c72e21c2ce8e0c9dde
Josh Lewis <josh.w.lewis@gmail.com>
2013-04-18 11:10:21 -0500

    Allow shorter passwords, and modify blacklist accordingly

--- jquery.entropy.js
+++ jquery.entropy.js
@@ -1,13 +1,13 @@
-/** 
+/**
 * Password Entropy
-* 
+*
 * This jQuery plug-in is built to give an estimate of the entropy of a password.
 * Initial calculations assume a randomly generated password is used, and then
 * applies a heuristics approach to penalize some common problems that arise
 * with human-generated passwords.
-* 
+*
 * Some of the patterns used for creating the estimates are based on data collected
-* in the paper "Testing Metrics for Password Creation Policies by Attacking Large 
+* in the paper "Testing Metrics for Password Creation Policies by Attacking Large
 * Sets of Revealed Passwords" by Weir et. al. and can be recommended as further
 * reading for those interested.
 *
@@ -17,17 +17,17 @@
 *
 * MIT LICENSE
 * Copyright (C) 2011 by Erik Brännström
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -61,10 +61,10 @@
                         return entropy;
                 },
                 function(entropy, password) {
-                    // Make sure password is not in blacklist 
+                    // Make sure password is not in blacklist
                     if(settings.blacklist.length == 0 || jQuery.inArray(password.toLowerCase(), settings.blacklist) === -1)
                         return entropy;
-                    
+
                     return log2(settings.blacklist.length);
                 },
                 function(entropy, password) {
@@ -78,11 +78,10 @@
                     return entropy - repeats*3;
                 }
             ],
-            'strings'   : ['Very weak', 'Weak', 'Pass', 'Strong', 'Very strong', 'Super strong'],
-            'classes'   : ['very-weak', 'weak', 'pass', 'strong', 'very-strong', 'super-strong'],
-            // Default blacklist contains 598 passwords that are at least 8 characters long.
-            // Based on Twitter's disallowed passwords, John the Ripper dictionary and most common RockYou passwords.
-            'blacklist' : ['password','computer','internet','baseball','michelle','changeme','trustno1','12345678','butthead','football','iloveyou','jennifer','jonathan','poohbear','sunshine','victoria','whatever','alexander','asdfjkl;','basketball','elephant','goodluck','lacrosse','ncc1701d','shithead','stephanie','a1b2c3d4','christin','franklin','kingfish','maryjane','maverick','mitchell','mountain','pearljam','princess','startrek','steelers','sunflower','superman','babylon5','benjamin','bluebird','chocolate','cocacola','einstein','explorer','flamingo','katherine','porsche911','security','snowball','thunderbird','training','alexandr','beautiful','caroline','challenge','charlotte','coltrane','crawford','elizabeth','firebird','fletcher','fountain','gabriell','garfield','godzilla','grateful','greenday','icecream','informix','jeanette','kathleen','lionking','majordomo','margaret','mariposa','marlboro','mercedes','metallic','monopoly','ncc1701e','nicholas','penelope','phoenix1','remember','republic','research','samantha','scarlett','snickers','snoopdog','spitfire','starwars','stingray','sumuinen','valentine','veronica','warriors','zeppelin','!@#$%^&*','broadway','raistlin','abcd1234','acropolis','asdfasdf','asdfghjk','babydoll','beatrice','blowfish','bluefish','bullshit','business','california','cannondale','carebear','catalina','catherine','champion','chelsea1','chester1','christian','colorado','columbia','commander','cordelia','creative','danielle','database','deadhead','dickhead','dragonfly','electric','excalibur','feedback','francesco','francine','francois','fuckface','gargoyle','goldfish','gretchen','harrison','idontknow','intrepid','jethrotull','johanna1','jordan23','kangaroo','kimberly','lawrence','liverpool','marathon','michael1','midnight','montreal','mortimer','nirvana1','notebook','overkill','patricia','pinkfloyd','predator','prometheus','rastafarian','reynolds','ricardo1','roadrunner','robinhood','robotech','rocknroll','salasana','sapphire','scarecrow','skywalker','smashing','snowflake','strawberry','sundance','superfly','swimming','teddybear','temporal','terminal','thejudge','thursday','valhalla','warcraft','williams','windsurf','woofwoof','wrangler','xcountry','11111111','88888888','bismillah','cardinal','front242','geronimo','madeline','sidekick','sterling','waterloo','wolverine','aardvark','aerobics','airborne','allstate','altamira','anderson','andromed','anything','applepie','aquarius','asdf1234','asdf;lkj','assmunch','barnyard','bernardo','birthday','blackjack','blueeyes','bluejean','brewster','butterfly','calendar','campbell','catwoman','chainsaw','chameleon','chinacat','chouette','chris123','christmas','christopher','clarkson','clueless','concorde','confused','coolbean','cornflake','corvette','crescent','crusader','cunningham','daedalus','damogran','darkstar','datatrain','december','deeznuts','dillweed','director','dominique','dontknow','downtown','dutchess','enterprise','fairview','ferguson','fireball','fishhead','flanders','florida1','flowerpot','frederic','freebird','froggies','frontier','gammaphi','garfunkel','gateway2','germany1','gilgamesh','halloween','hallowell','hamilton','happy123','happyday','hardcore','hawkeye1','heather1','heather2','hedgehog','hello123','hellohello','heythere','highland','histoire','hongkong','hosehead','hydrogen','indonesia','instruct','integral','isabelle','jamesbond','jeepster','jeffrey1','justdoit','justice4','kalamazo','katerina','kittycat','kristine','laserjet','lissabon','loislane','lonestar','longhorn','makeitso','manageme','marielle','marshall','mattingly','meatloaf','mechanic','michigan','microsoft','millenium','mobydick','montana3','montrose','moonbeam','morecats','morpheus','motorola','munchkin','mustang1','napoleon','national','neutrino','newaccount','newyork1','nicklaus','nightshadow','nightwind','nintendo','obsession','paradigm','patriots','performa','peterpan','phialpha','phillips','pianoman','pipeline','precious','printing','provider','qwerty12','qwertyui','rachelle','redcloud','redskins','renegade','revolution','rhjrjlbk','richard1','richards','richmond','robotics','rootbeer','rossigno','ruthless','saturday','schnapps','scoobydoo','scooter1','scorpion','september','services','shanghai','sigmachi','signature','skipper1','sprocket','starbuck','stargate','starlight','stranger','student2','superstar','sweetpea','swordfish','tacobell','tazdevil','testtest','thankyou','thelorax','thisisit','thompson','thrasher','tightend','tinkerbell','transfer','transport','treasure','trombone','ultimate','vacation','vincent1','virginia','webmaster','whocares','whoville','william1','winniethepooh','wolfgang','xxxxxxxx','yogibear','00000000','1234qwer','21122112','99999999','anaconda','apollo13','blizzard','carolina','chandler','changeit','charlie1','chiquita','chocolat','christia','christoph','classroom','courtney','dolphins','fearless','good-luck','graymail','guinness','homebrew','lorraine','macintosh','nebraska','newcourt','politics','portland','property','softball','stephani','valentin','zhongguo','access14','bigdaddy','mistress','password1','password12','password123','redwings','rush2112','srinivas','123456789','babygirl','1234567890','987654321','spongebob','princesa','alexandra','estrella','princess1','alejandro','brittany','alejandra','tequiero','blink182','fernando','cristina','babygurl','november','mahalkita','gabriela','iloveyou2','pictures','hellokitty','babygirl1','angelica','iloveyou1','inuyasha','sebastian','spiderman','0123456789','barcelona','slipknot','cutiepie','789456123','portugal','volleyball','rockstar','cristian','chrisbrown','lollipop','qwertyuiop','harrypotter','ihateyou','christine','johncena','lovelove','metallica','myspace1','babyblue','fernanda','westlife','slideshow','asdfghjkl','santiago','sweetheart','12345678910','leonardo','sexygirl','anthony1','skittles','brooklyn','colombia','christina','teiubesc','147258369','francisco','amorcito','angelito','manchester','linkinpark','fuckyou1','bestfriend','sporting','truelove','savannah','scotland','ilovehim','estrellita','brandon1','loverboy','emmanuel','999999999','westside','mauricio','preciosa','shopping','isabella','martinez','friendster','valentina','fuckyou2','sunshine1','gangster','darkangel','bettyboop','jessica1','cheyenne','bestfriends','daddysgirl','billabong','buttercup','zacefron','tokiohotel','bubblegum','darkness','lollypop','sexybitch','hotstuff','babylove','angelina','playgirl','football1','milagros','margarita','undertaker','capricorn','cheerleader','password2','matthew1','carlitos','michelle1','cinderella','jesuschrist','ilovejesus','tazmania','princesita','jesucristo','lipgloss','741852963','hernandez','pussycat','gorgeous','simpsons','panthers','hollywood','ilovegod','kristina','sexymama','scarface','0987654321','jeremiah','pineapple','butterfly1']
+            'strings'   : ['Very Weak', 'Weak', 'Acceptable', 'Good', 'Strong', 'Very Strong'],
+            'classes'   : ['very-weak', 'weak', 'Acceptable', 'good', 'strong', 'very-strong'],
+            // Default blacklist contains 504 based on Twitters disallowed and the 500 worst passwords.
+            'blacklist' : ["111111", "11111111", "112233", "121212", "123123", "123456", "1234567", "12345678", "131313", "232323", "654321", "666666", "696969", "777777", "7777777", "8675309", "987654", "aaaaaa", "abc123", "abcdef", "abgrtyu", "access", "access14", "action", "albert", "alexis", "amanda", "amateur", "andrea", "andrew", "angela", "angels", "animal", "anthony", "apollo", "apples", "arsenal", "arthur", "asdfgh", "ashley", "august", "austin", "badboy", "bailey", "banana", "barney", "baseball", "batman", "beaver", "beavis", "bigdaddy", "bigdog", "birdie", "bitches", "biteme", "blazer", "blonde", "blondes", "bond007", "bonnie", "booboo", "booger", "boomer", "boston", "brandon", "brandy", "braves", "brazil", "bronco", "broncos", "bulldog", "buster", "butter", "butthead", "calvin", "camaro", "cameron", "canada", "captain", "carlos", "carter", "casper", "charles", "charlie", "cheese", "chelsea", "chester", "chicago", "chicken", "cocacola", "coffee", "college", "compaq", "computer", "cookie", "cooper", "corvette", "cowboy", "cowboys", "crystal", "dakota", "dallas", "daniel", "danielle", "debbie", "dennis", "diablo", "diamond", "doctor", "doggie", "dolphin", "dolphins", "donald", "dragon", "dreams", "driver", "eagle1", "eagles", "edward", "einstein", "erotic", "extreme", "falcon", "fender", "ferrari", "firebird", "fishing", "florida", "flower", "flyers", "football", "forever", "freddy", "freedom", "gandalf", "gateway", "gators", "gemini", "george", "giants", "ginger", "golden", "golfer", "gordon", "gregory", "guitar", "gunner", "hammer", "hannah", "hardcore", "harley", "heather", "helpme", "hockey", "hooters", "horney", "hotdog", "hunter", "hunting", "iceman", "iloveyou", "internet", "iwantu", "jackie", "jackson", "jaguar", "jasmine", "jasper", "jennifer", "jeremy", "jessica", "johnny", "johnson", "jordan", "joseph", "joshua", "junior", "justin", "killer", "knight", "ladies", "lakers", "lauren", "leather", "legend", "letmein", "little", "london", "lovers", "maddog", "madison", "maggie", "magnum", "marine", "marlboro", "martin", "marvin", "master", "matrix", "matthew", "maverick", "maxwell", "melissa", "member", "mercedes", "merlin", "michael", "michelle", "mickey", "midnight", "miller", "mistress", "monica", "monkey", "monster", "morgan", "mother", "mountain", "muffin", "murphy", "mustang", "naked", "nascar", "nathan", "naughty", "ncc1701", "newyork", "nicholas", "nicole", "nipple", "nipples", "oliver", "orange", "packers", "panther", "panties", "parker", "password", "password1", "password12", "password123", "patrick", "peaches", "peanut", "pepper", "phantom", "phoenix", "player", "please", "pookie", "porsche", "prince", "princess", "private", "purple", "pussies", "qazwsx", "qwerty", "qwertyui", "rabbit", "rachel", "racing", "raiders", "rainbow", "ranger", "rangers", "rebecca", "redskins", "redsox", "redwings", "richard", "robert", "rocket", "rosebud", "runner", "rush2112", "russia", "samantha", "sammy", "samson", "sandra", "saturn", "scooby", "scooter", "scorpio", "scorpion", "secret", "sexsex", "shadow", "shannon", "shaved", "sierra", "silver", "skippy", "slayer", "smokey", "snoopy", "soccer", "sophie", "spanky", "sparky", "spider", "squirt", "srinivas", "startrek", "starwars", "steelers", "steven", "sticky", "stupid", "success", "summer", "sunshine", "superman", "surfer", "swimming", "sydney", "taylor", "tennis", "teresa", "tester", "testing", "theman", "thomas", "thunder", "thx1138", "tiffany", "tigers", "tigger", "tomcat", "topgun", "toyota", "travis", "trouble", "trustno1", "tucker", "turtle", "twitter", "united", "vagina", "victor", "victoria", "viking", "voodoo", "voyager", "walter", "warrior", "welcome", "whatever", "william", "willie", "wilson", "winner", "winston", "winter", "wizard", "xavier", "xxxxxx", "xxxxxxxx", "yamaha", "yankee", "yankees", "yellow", "zxcvbn", "zxcvbnm", "zzzzzz", "1234", "pussy", "12345", "pass", "fuckme", "6969", "fuck", "2000", "test", "love", "sexy", "asshole", "fuckyou", "1111", "enter", "fucker", "blowjob", "dick", "bitch", "hello", "black", "money", "horny", "girls", "john", "james", "mike", "blowme", "chris", "david", "fucking", "bigdick", "blue", "house", "jack", "golf", "bear", "tiger", "angel", "porno", "1212", "fish", "porn", "teens", "jason", "cumshot", "lover", "5150", "bubba", "2112", "fred", "xxxxx", "tits", "boobs", "penis", "white", "bigtits", "green", "super", "magic", "scott", "2222", "asdf", "video", "7777", "bill", "peter", "cock", "beer", "beach", "star", "frank", "dave", "11111", "steve", "viper", "ou812", "jake", "suckit", "buddy", "young", "lucky", "baby", "cunt", "brian", "mark", "4444", "bigcock", "happy", "booty", "fucked", "0", "fire", "chevy", "slut", "power", "paris", "rock", "xxxx", "dirty", "ford", "wolf", "alex", "eric", "movie", "great", "cool", "1313", "japan", "stars", "apple", "aaaa", "kevin", "matt", "4321", "4128", "shit", "3333", "cumming", "kitty", "cream", "kelly", "paul", "mine", "king", "5555", "eagle", "hentai", "smith", "enjoy", "girl", "qwert", "time", "women", "juice", "music", "billy", "6666"]
         };
 
         // Recursively merge user options with default settings and fix array merge
@@ -118,20 +117,20 @@
                 for (var i in settings.functions) {
                     entropy = settings.functions[i](entropy, psw);
                 }
-                
+
                 // Set message display
                 var res = 0;
-                if(entropy >= 78)
+                if(entropy >= 60)
                     res = 5;
-                else if(entropy >= 66)
-                    res = 4;
-                else if(entropy >= 56)
-                    res = 3;
                 else if(entropy >= 48)
+                    res = 4;
+                else if(entropy >= 36)
+                    res = 3;
+                else if(entropy >= 24)
                     res = 2;
-                else if(entropy >= 40)
+                else if(entropy >= 12)
                     res = 1;
-                
+
                 // Display results
                 $(settings.display).removeClass(settings.classes.join(' '))
                         .addClass(settings.classes[res])
